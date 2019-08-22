876778333e3a5f104ee246ef1b4972ee80c0ac06
madhavipuliraju <madhavi@vlead.vlead>
2019-08-22 16:01:53 +0530

    linked list artefacts

---
+++ code/runtime/js/realization.js
@@ -0,0 +1,187 @@
+
+var realizationCatalog = {	
+  "Structure": {
+    "resource_type": "image",
+    "url": "/build/code/static/img/structure.jpg",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "What is Linked List?":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/linked-list-example.jpg",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Linked List vs Array":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/llintro.png",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Types of Linked List":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/types-of-ll.png",
+    "width": "40%",
+    "height": "100%"
+  },
+
+  "Pictorial Representation of SLL":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/real-life.png",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Representation of SLL":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/linkedlist.png",
+  },
+
+  "Insertion (1) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Insertion (1).png",
+  },
+
+  "Insertion (2a) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Insertion (2a).png",
+  },
+
+  "Insertion (2b) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Insertion (2b).png",
+  },
+
+  "Insertion (2c) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Insertion (2c).png",
+    "height": "70%"
+  },
+
+  "Deletion (2) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Deletion (2).png",
+  },
+
+  "Deletion (3a) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Deletion (3a).png",
+  },
+
+  "Deletion (3b) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Deletion (3b).png",
+    "height": "70%"
+  },
+
+  "Deletion (3c) image":{
+    "resource_type": "image",
+    "url": "/build/code/static/images/Deletion (3c).png",
+  },
+
+  "Singly Linked List Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/sll_intro.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Insertion Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/sll_insert.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Search Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/sll_search.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Delete Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/sll_remove.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Practice SLL":{
+    "reqs_satisfied": "Provides an artifact to practice sll",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/sllpractice.html"
+  },
+
+  "Exercise SLL":{
+    "reqs_satisfied": "Provides an artifact to exercise sll",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/sllexercise.html"
+  },
+
+  "Quiz DLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/dllquiz_html.html"
+  },
+
+  "Pictorial Representation of DLL":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/doublyll.jpg",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "DLL Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/dll_demo.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Practice DLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/dllpractice.html"    
+  },
+
+  "Exercise DLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/dllexercise.html"    
+  },
+
+  "Quiz DLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/dllquiz_html.html"    
+  },
+
+  "Pictorial Representation of CLL":{
+    "resource_type": "image",
+    "url": "/build/code/static/img/circularly_linked_lists.png",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "CLL Video":{
+    "resource_type": "video",
+    "url": "/build/code/static/videos/cll_demo.mp4",
+    "width": "40%",
+    "height": "40vh"
+  },
+
+  "Practice CLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/cllpractice.html"
+  },
+
+  "Exercise CLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/cllexercise.html"  
+  },
+  
+  "Quiz CLL":{
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/cllquiz_html.html"  
+  }
+};
